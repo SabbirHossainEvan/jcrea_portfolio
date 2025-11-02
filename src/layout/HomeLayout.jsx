c690c8460f0c6618deb734fd86a0ad1cb84271d7
SabbirHossainEvan <evansabbir31@gmail.com>
2025-11-02 12:09:52 +0600

    section added

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className='text-primary'>
             HomeLayout
+            
         </div>
     );
 };
